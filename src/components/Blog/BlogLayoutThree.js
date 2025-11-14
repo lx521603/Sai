36441211082659874b8788fc032f5f1ad00912d8
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 20:32:31 +0800

    更新 BlogLayoutThree.js

--- src/components/Blog/BlogLayoutThree.js
+++ src/components/Blog/BlogLayoutThree.js
@@ -7,39 +7,45 @@ const BlogLayoutThree = ({ blog }) => {
   return (
     <div className="group flex flex-col items-center text-dark dark:text-light">
       <Link href={blog.url} className="h-full rounded-xl overflow-hidden">
-        <Image
-          src={blog.image.src}
-          placeholder="blur"
-          blurDataURL={blog.image.blurDataURL}
-          alt={blog.title}
-          width={blog.image.width}
-          height={blog.image.height}
-          className=" aspect-[4/3] w-full h-full object-cover object-center  group-hover:scale-105 transition-all ease duration-300 "
-          sizes="(max-width: 640px) 100vw,(max-width: 1024px) 50vw, 33vw"
-        />
+        {blog.image && (
+          <Image
+            src={blog.image.src}
+            placeholder={blog.image.blurDataURL ? "blur" : undefined}
+            blurDataURL={blog.image.blurDataURL}
+            alt={blog.title}
+            width={blog.image.width}
+            height={blog.image.height}
+            className="aspect-[4/3] w-full h-full object-cover object-center group-hover:scale-105 transition-all ease duration-300"
+            sizes="(max-width: 640px) 100vw,(max-width: 1024px) 50vw, 33vw"
+          />
+        )}
       </Link>
 
       <div className="flex flex-col w-full mt-4">
-        <span className="uppercase text-accent dark:text-accentDark font-semibold text-xs sm:text-sm">
-          {blog.tags[0]}
-        </span>
+        {blog.tags && blog.tags.length > 0 && (
+          <span className="uppercase text-accent dark:text-accentDark font-semibold text-xs sm:text-sm">
+            {blog.tags[0]}
+          </span>
+        )}
 
         <Link href={blog.url} className="inline-block my-1">
-          <h2 className="font-semibold capitalize  text-base sm:text-lg">
+          <h2 className="font-semibold capitalize text-base sm:text-lg">
             <span
-              className="bg-gradient-to-r from-accent/50 to-accent/50  dark:from-accentDark/50
+              className="bg-gradient-to-r from-accent/50 to-accent/50 dark:from-accentDark/50
               dark:to-accentDark/50
               bg-[length:0px_6px]
-              group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 "
+              group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500"
             >
               {blog.title}
             </span>
           </h2>
         </Link>
 
-        <span className="capitalize text-gray dark:text-light/50 font-semibold text-sm  sm:text-base">
-          {format(new Date(blog.publishedAt), "MMMM dd, yyyy")}
-        </span>
+        {blog.publishedAt && (
+          <span className="capitalize text-gray dark:text-light/50 font-semibold text-sm sm:text-base">
+            {format(new Date(blog.publishedAt), "MMMM dd, yyyy")}
+          </span>
+        )}
       </div>
     </div>
   );
