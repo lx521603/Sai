56cfc51d4e56521ab27085ba795b8ac6b3614e3b
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 21:53:41 +0800

    更新 index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -91,8 +91,8 @@ const Footer = () => {
         </Link>
         <div className="text-center">
           Made with &hearts; by{" "}
-          <a href="https://devdreaming.com" className="underline" target="_blank">
-            CodeBucks
+          <a href="https://sai.st" className="underline" target="_blank">
+            Sai
           </a>
         </div>
       </div>
