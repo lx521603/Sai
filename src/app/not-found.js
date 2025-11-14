c8e75ddb8937950495f3740f3892ea1f4d902623
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 21:43:52 +0800

    更新 not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -7,7 +7,7 @@ export default function NotFound() {
         <h1 className={`inline-block text-dark dark:text-light
       text-6xl font-bold w-full capitalize xl:text-8xl text-center`}>404</h1>
         <h2 className={`inline-block text-dark dark:text-light
-      text-5xl font-bold w-full capitalize xl:text-6xl text-center mt-4 tracking-wide leading-snug`}>Page Not Found!</h2>
+      text-5xl font-bold w-full capitalize xl:text-6xl text-center mt-4 tracking-wide leading-snug`}>页面没找到!</h2>
         <Link
           href="/"
           className="self-center mt-8 inline-block rounded-lg border-2 border-solid bg-dark px-4 py-2
@@ -15,7 +15,7 @@ export default function NotFound() {
         dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
         "
         >
-          Go To Home
+          返回首页
         </Link>
       </div>
     </main>
