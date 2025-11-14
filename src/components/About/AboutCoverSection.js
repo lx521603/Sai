45784421bae55ccc23ad4def52d100c2e6059078
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 20:43:00 +0800

    更新 AboutCoverSection.js

--- src/components/About/AboutCoverSection.js
+++ src/components/About/AboutCoverSection.js
@@ -15,10 +15,10 @@ const AboutCoverSection = () => {
 
         <div className='w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
             <h2 className='font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left'>
-            Dream Big, Work Hard, Achieve More!
+            Dream Big, Work Hard, Achieve More!敢于梦想，努力奋斗，成就更多！
             </h2>
             <p className='font-medium capitalize mt-4 text-base'>
-            This Mantra Drives My Work As A Passionate Freelancer. I Blend Innovative Technology With Timeless Design For Captivating Digital Experiences. Inspired By Nature And Literature, I'm A Perpetual Learner Embracing Challenges. With Each Project, I Aim To Leave A Lasting Impact—One Pixel At A Time.
+            这句箴言驱动着我作为自由职业者的热情。我将创新科技与永恒设计融合，创造引人入胜的数字体验。受自然与文学启发，我始终保持学习者的姿态，勇于迎接挑战。每一个项目，我都力求留下持久的影响——一像素一积累。
             </p>
         </div>
     </section>
