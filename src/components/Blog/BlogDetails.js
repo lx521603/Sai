83a20bfc15ebf8f6b1f8bef77f60604cac23208d
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 20:30:39 +0800

    更新 BlogDetails.js

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -6,17 +6,24 @@ import ViewCounter from "./ViewCounter";
 
 const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
-    <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
+    <div className="px-2 md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg">
       <time className="m-3">
-        {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
+        {blog.publishedAt
+          ? format(parseISO(blog.publishedAt), "LLLL d, yyyy")
+          : ""}
       </time>
       <span className="m-3">
         <ViewCounter slug={blogSlug} />
       </span>
-      <div className="m-3">{blog.readingTime.text}</div>
-      <Link href={`/categories/${slug(blog.tags[0])}`} className="m-3">
-        #{blog.tags[0]}
-      </Link>
+      <div className="m-3">{blog.readingTime?.text || ""}</div>
+      {blog.tags && blog.tags.length > 0 && (
+        <Link
+          href={`/categories/${slug(blog.tags[0])}`}
+          className="m-3"
+        >
+          #{blog.tags[0]}
+        </Link>
+      )}
     </div>
   );
 };
