9ffc0d74f46ae8f328d060977040b53e71b71827
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 22:16:59 +0800

    更新 RecentPosts.js

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -10,13 +10,13 @@ const RecentPosts = ({ blogs }) => {
     <section className="w-full  mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
       <div className="w-full flex  justify-between">
         <h2 className="w-fit inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
-          Recent Posts
+          近期文章
         </h2>
         <Link
           href="/categories/all"
           className="inline-block font-medium text-accent dark:text-accentDark underline underline-offset-2      text-base md:text-lg"
         >
-          view all
+          浏览所有
         </Link>
       </div>
 
