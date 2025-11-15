cf13bf6db445bf0c7d67306972a5105cd54871f6
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-15 16:18:19 +0800

    更新 BlogLayoutTwo.js

--- src/components/Blog/BlogLayoutTwo.js
+++ src/components/Blog/BlogLayoutTwo.js
@@ -30,7 +30,7 @@ const BlogLayoutTwo = ({ blog }) => {
           blog.tagSlugs &&
           blog.tagSlugs.length > 0 && (
             <Link
-              href={`/categories/${encodeURIComponent(blog.tagSlugs[0])}`} // ✅ encode
+              href={`/categories/${blog.tagSlugs[0]}`} // ✅ 移除 encodeURIComponent
               className="inline-block w-full uppercase text-accent dark:text-accentDark font-semibold text-xs sm:text-sm"
             >
               {blog.tags[0]}
