e0a4288583f1cc6285a4562ccd91e5598104cbc6
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-15 18:21:13 +0800

    更新 siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -4,18 +4,16 @@ const siteMetadata = {
     author: 'Sai',
     headerTitle: '童心未泯',
     description: '这里是描述',
-    language: 'zj-CN',
+    language: 'zh-CN',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://sai.st', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'x@sai.st', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
+    github: 'https://github.com/lx521603',
+    twitter: 'https://twitter.com/oniyt1980',
     facebook: 'https://facebook.com',
     youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
     locale: 'zh-CN',
   }
   
