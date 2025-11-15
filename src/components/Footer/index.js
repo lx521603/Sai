4b46c320806696b358e191f7c0ee1e425ada45c8
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-15 20:17:57 +0800

    更新 index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,7 +103,7 @@ const Footer = () => {
 
       {/* 底部版权 */}
       <div className="w-full mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex flex-col md:flex-row items-center justify-between">
-        <span className="text-center">&copy;2023 CodeBucks. All rights reserved.</span>
+        <span className="text-center">&copy;2025 sai.st. All rights reserved.</span>
         <Link href="/sitemap.xml" className="text-center underline my-4 md:my-0">
           sitemap.xml
         </Link>
