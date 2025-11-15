27d2c015a7a783aa2825744be35c1fbbb6f12d70
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-16 00:09:51 +0800

    更新 FeaturedPosts.js

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -4,7 +4,7 @@ import BlogLayoutOne from "../Blog/BlogLayoutOne";
 import BlogLayoutTwo from "../Blog/BlogLayoutTwo";
 
 const FeaturedPosts = ({ blogs }) => {
-  const sortedBlogs = sortBlogs(blogs);
+  const featuredBlogs = blogs.filter(blog => blog.featured);
 
   return (
     <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
