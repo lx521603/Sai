de7e8514df556699d1866b7e095d9c44d9ed2fa0
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-15 14:20:01 +0800

    更新 FeaturedPosts.js

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -5,23 +5,32 @@ import BlogLayoutTwo from "../Blog/BlogLayoutTwo";
 
 const FeaturedPosts = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
-  return <section className="w-full mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
-    <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">精选文章</h2>
 
-    <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
-      <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
-        <BlogLayoutOne blog={sortedBlogs[1]} />
-      </article>
-      <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutTwo blog={sortedBlogs[2]} />
+  return (
+    <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
+      <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
+        精选文章
+      </h2>
 
-      </article>
-      <article className="col-span-2 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutTwo blog={sortedBlogs[3]} />
-
-      </article>
-    </div>
-  </section>;
+      <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-10 sm:mt-16">
+        {sortedBlogs[0] && (
+          <article className="col-span-2 sxl:col-span-1 row-span-2 relative">
+            <BlogLayoutOne blog={sortedBlogs[0]} />
+          </article>
+        )}
+        {sortedBlogs[1] && (
+          <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+            <BlogLayoutTwo blog={sortedBlogs[1]} />
+          </article>
+        )}
+        {sortedBlogs[2] && (
+          <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+            <BlogLayoutTwo blog={sortedBlogs[2]} />
+          </article>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedPosts;
