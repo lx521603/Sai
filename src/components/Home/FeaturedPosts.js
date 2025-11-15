7d0062681552226895e7a384fab797d4f818d9e2
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-16 01:00:41 +0800

    更新 FeaturedPosts.js

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -1,48 +1,80 @@
 import { sortBlogs } from "@/src/utils";
-import Link from "next/link";
 import React from "react";
-import BlogLayoutThree from "../Blog/BlogLayoutThree";
+import BlogLayoutOne from "../Blog/BlogLayoutOne";
+import BlogLayoutTwo from "../Blog/BlogLayoutTwo";
 import Tag from "../Elements/Tag";
 
-const RecentPosts = ({ blogs }) => {
+const FeaturedPosts = ({ blogs }) => {
+  // 防御性：无数据不渲染
   if (!Array.isArray(blogs) || blogs.length === 0) return null;
 
+  // 优先筛选 frontmatter 里标记了 featured:true 的文章
+  const featuredBlogs = blogs.filter((blog) => blog.featured);
+
+  // 如果没有标记，就用排序后的前 3 篇
   const sortedBlogs = sortBlogs(blogs);
+  const list = featuredBlogs.length > 0 ? featuredBlogs : sortedBlogs.slice(0, 3);
+
+  // 再次防御性：确保有内容
+  if (list.length === 0) return null;
 
   return (
     <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
-      <div className="w-full flex justify-between">
-        <h2 className="w-fit inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
-          近期文章
-        </h2>
-        <Link
-          href="/categories/all"
-          className="inline-block font-medium text-accent dark:text-accentDark underline underline-offset-2 text-base md:text-lg"
-        >
-          浏览所有
-        </Link>
-      </div>
+      <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
+        精选文章
+      </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-16 mt-16">
-        {sortedBlogs.slice(4, 10).map((blog) =>
-          blog ? (
-            <article key={blog._id || blog.slug} className="col-span-1 row-span-1 relative">
-              <BlogLayoutThree blog={blog} />
+      <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-10 sm:mt-16">
+        {/* 第一篇：大图布局 */}
+        {list[0] && (
+          <article className="col-span-2 sxl:col-span-1 row-span-2 relative">
+            <BlogLayoutOne blog={list[0]} />
 
-              {/* ✅ 显示所有标签 */}
-              {blog.tags && blog.tagSlugs && blog.tags.length > 0 && (
-                <div className="flex flex-wrap gap-2 mt-2">
-                  {blog.tags.map((tag, idx) => (
-                    <Tag key={idx} link={`/categories/${blog.tagSlugs[idx]}`} name={tag} />
-                  ))}
-                </div>
-              )}
-            </article>
-          ) : null
+            {/* 显示所有标签 */}
+            {list[0].tags && list[0].tagSlugs && list[0].tags.length > 0 && (
+              <div className="flex flex-wrap gap-2 mt-2">
+                {list[0].tags.map((tag, idx) => (
+                  <Tag key={idx} link={`/categories/${list[0].tagSlugs[idx]}`} name={tag} />
+                ))}
+              </div>
+            )}
+          </article>
+        )}
+
+        {/* 第二篇：小图布局 */}
+        {list[1] && (
+          <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+            <BlogLayoutTwo blog={list[1]} />
+
+            {/* 显示所有标签 */}
+            {list[1].tags && list[1].tagSlugs && list[1].tags.length > 0 && (
+              <div className="flex flex-wrap gap-2 mt-2">
+                {list[1].tags.map((tag, idx) => (
+                  <Tag key={idx} link={`/categories/${list[1].tagSlugs[idx]}`} name={tag} />
+                ))}
+              </div>
+            )}
+          </article>
+        )}
+
+        {/* 第三篇：小图布局 */}
+        {list[2] && (
+          <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+            <BlogLayoutTwo blog={list[2]} />
+
+            {/* 显示所有标签 */}
+            {list[2].tags && list[2].tagSlugs && list[2].tags.length > 0 && (
+              <div className="flex flex-wrap gap-2 mt-2">
+                {list[2].tags.map((tag, idx) => (
+                  <Tag key={idx} link={`/categories/${list[2].tagSlugs[idx]}`} name={tag} />
+                ))}
+              </div>
+            )}
+          </article>
         )}
       </div>
     </section>
   );
 };
 
-export default RecentPosts;
+export default FeaturedPosts;
