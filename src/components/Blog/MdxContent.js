e91478a52c16b2a0b8b1ee7b7c5899dfc36dcd12
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-16 21:20:43 +0800

    更新 MdxContent.js

--- src/components/Blog/MdxContent.js
+++ src/components/Blog/MdxContent.js
@@ -1,18 +1,48 @@
-import * as runtime from 'react/jsx-runtime'
-import Image from 'next/image'
+import * as runtime from "react/jsx-runtime";
+import Image from "next/image";
+import { useState } from "react";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
-const sharedComponents = {
-  Image
+// 自定义图片预览组件
+function ImageWithLightbox({ src, alt }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <img
+        src={src}
+        alt={alt}
+        className="cursor-pointer"
+        onClick={() => setOpen(true)}
+      />
+      {open && (
+        <Lightbox
+          mainSrc={src}
+          onCloseRequest={() => setOpen(false)}
+          imageCaption={alt}
+        />
+      )}
+    </>
+  );
 }
+
+// 注册共享组件：覆盖 img，保留 next/image
+const sharedComponents = {
+  Image,
+  img: (props) => <ImageWithLightbox {...props} />,
+};
 
 const useMDXComponent = (code) => {
-  const fn = new Function(code)
-  return fn({ ...runtime }).default
-}
+  const fn = new Function(code);
+  return fn({ ...runtime }).default;
+};
 
- const MDXContent = ({ code, components, ...props }) => {
-  const Component = useMDXComponent(code)
-  return <Component components={{ ...sharedComponents, ...components }} {...props} />
-}
+const MDXContent = ({ code, components, ...props }) => {
+  const Component = useMDXComponent(code);
+  return (
+    <Component components={{ ...sharedComponents, ...components }} {...props} />
+  );
+};
 
-export default MDXContent
+export default MDXContent;
