c2b0dd63ae0cfe309a171bf693299a65adfe3c48
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-16 21:50:34 +0800

    更新 mdxComponents.js

--- src/components/Elements/mdxComponents.js
+++ src/components/Elements/mdxComponents.js
@@ -1,7 +1,44 @@
-import Tag from './Tag'
-import Location from './Location'
+'use client';
 
-export const components = {
-  Tag,
-  Location,
+import * as runtime from "react/jsx-runtime";
+import Image from "next/image";
+import { useState } from "react";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+import Tag from './Tag'
+import Location from "./Location"; // 路径要和你项目实际一致
+
+function ImageWithLightbox({ src, alt }) {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <img src={src} alt={alt} className="cursor-pointer" onClick={() => setOpen(true)} />
+      {open && (
+        <Lightbox
+          mainSrc={src}
+          onCloseRequest={() => setOpen(false)}
+          imageCaption={alt}
+        />
+      )}
+    </>
+  );
 }
+
+const sharedComponents = {
+  Tag,
+  Image,
+  Location, // ✅ 确保这里注册了
+  img: (props) => <ImageWithLightbox {...props} />,
+};
+
+const useMDXComponent = (code) => {
+  const fn = new Function(code);
+  return fn({ ...runtime }).default;
+};
+
+const MDXContent = ({ code, components, ...props }) => {
+  const Component = useMDXComponent(code);
+  return <Component components={{ ...sharedComponents, ...components }} {...props} />;
+};
+
+export default MDXContent;
