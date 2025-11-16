021fdb3d34be7178fbd2f99952298358c74960d3
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-17 01:19:32 +0800

    更新 MdxContent.js

--- src/components/Blog/MdxContent.js
+++ src/components/Blog/MdxContent.js
@@ -10,55 +10,61 @@ import Zoom from 'yet-another-react-lightbox/plugins/zoom'
 import 'yet-another-react-lightbox/styles.css'
 import 'yet-another-react-lightbox/plugins/thumbnails.css'
 
-// 自定义图片点击放大组件
-function ImageWithLightbox({ src, alt }) {
+const MDXContent = ({ code, components, ...props }) => {
   const [open, setOpen] = useState(false)
+  const [index, setIndex] = useState(0)
+  const [images, setImages] = useState([])
 
-  return (
-    <>
+  // 自定义图片点击放大组件，收集所有图片
+  const ImageWithLightbox = ({ src, alt }) => {
+    const currentIndex = images.length
+    // 每次渲染时把图片加入数组
+    images.push({ src, description: alt })
+
+    return (
       <img
         src={src}
         alt={alt}
         className="cursor-pointer"
-        onClick={() => setOpen(true)}
+        onClick={() => {
+          setIndex(currentIndex)
+          setOpen(true)
+        }}
       />
-      {open && (
-        <Lightbox
-          open={open}
-          close={() => setOpen(false)}
-          slides={[{ src, description: alt }]}
-          plugins={[Thumbnails, Zoom]}
-        />
-      )}
-    </>
-  )
-}
-
-// 保留 next/image，同时覆盖 img 为 Lightbox
-const sharedComponents = {
-  Image,
-  img: (props) => <ImageWithLightbox {...props} />,
-}
-
-const useMDXComponent = (code) => {
-  if (!code) return () => <div>⚠️ MDX code missing</div>
-  try {
-    const fn = new Function(code)
-    const Comp = fn({ ...runtime })
-    return Comp?.default || (() => <div>⚠️ Invalid MDX</div>)
-  } catch (err) {
-    console.error('MDX compile error:', err)
-    return () => <div>⚠️ MDX compile error</div>
+    )
   }
-}
 
-const MDXContent = ({ code, components, ...props }) => {
+  // 保留 next/image，同时覆盖 img 为 Lightbox
+  const sharedComponents = {
+    Image,
+    img: (props) => <ImageWithLightbox {...props} />,
+  }
+
+  const useMDXComponent = (code) => {
+    if (!code) return () => <div>⚠️ MDX code missing</div>
+    try {
+      const fn = new Function(code)
+      const Comp = fn({ ...runtime })
+      return Comp?.default || (() => <div>⚠️ Invalid MDX</div>)
+    } catch (err) {
+      console.error('MDX compile error:', err)
+      return () => <div>⚠️ MDX compile error</div>
+    }
+  }
+
   const Component = useMDXComponent(code)
+
   return (
-    <Component
-      components={{ ...sharedComponents, ...components }}
-      {...props}
-    />
+    <>
+      <Component components={{ ...sharedComponents, ...components }} {...props} />
+      <Lightbox
+        open={open}
+        index={index}
+        close={() => setOpen(false)}
+        slides={images}
+        plugins={[Thumbnails, Zoom]}
+      />
+    </>
   )
 }
 
